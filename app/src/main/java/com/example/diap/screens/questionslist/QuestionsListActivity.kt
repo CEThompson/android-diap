3950ce6dd6f81deda2a50864608d3e8f8feb1266
Erik <vwoomdev@gmail.com>
2020-12-24 15:49:50 -0800

    refactor: extract domain logic (networking logic) from questions list activity into use case

--- app/src/main/java/com/example/diap/screens/questionslist/QuestionsListActivity.kt
+++ app/src/main/java/com/example/diap/screens/questionslist/QuestionsListActivity.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import com.example.diap.Constants
 import com.example.diap.networking.StackoverflowApi
+import com.example.diap.questions.FetchQuestionsUseCase
 import com.example.diap.questions.Question
 import com.example.diap.screens.common.dialogs.ServerErrorDialogFragment
 import com.example.diap.screens.questiondetails.QuestionDetailsActivity
@@ -17,11 +18,11 @@ class QuestionsListActivity : AppCompatActivity(), QuestionsListViewMvc.Listener
 
     private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
 
-    private lateinit var stackoverflowApi: StackoverflowApi
-
     private var isDataLoaded = false
 
     private lateinit var viewMvc: QuestionsListViewMvc
+
+    private lateinit var fetchQuestionsUseCase: FetchQuestionsUseCase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,12 +31,7 @@ class QuestionsListActivity : AppCompatActivity(), QuestionsListViewMvc.Listener
 
         setContentView(viewMvc.rootView)
 
-        // init retrofit
-        val retrofit = Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-        stackoverflowApi = retrofit.create(StackoverflowApi::class.java)
+        fetchQuestionsUseCase = FetchQuestionsUseCase()
     }
 
     override fun onStart() {
@@ -65,18 +61,14 @@ class QuestionsListActivity : AppCompatActivity(), QuestionsListViewMvc.Listener
         coroutineScope.launch {
             viewMvc.showProgressIndication()
             try {
-                val response = stackoverflowApi.lastActiveQuestions(20)
-                if (response.isSuccessful && response.body() != null) {
-                    viewMvc.bindQuestions(response.body()!!.questions)
-                    isDataLoaded = true
-                } else {
-                    onFetchFailed()
-                    Log.d("QuestionsListActivity", "fetch failed ${response.message()}")
-                }
-            } catch (t: Throwable) {
-                if (t !is CancellationException) {
-                    onFetchFailed()
-                    Log.d("QuestionsListActivity", "throw ${t.localizedMessage}")
+                when (val result = fetchQuestionsUseCase.fetchLatestQuestions()) {
+                    is FetchQuestionsUseCase.Result.Success -> {
+                        viewMvc.bindQuestions(result.questions)
+                        isDataLoaded = true
+                    }
+                    is FetchQuestionsUseCase.Result.Failure -> {
+                        onFetchFailed()
+                    }
                 }
             } finally {
                 viewMvc.hideProgressIndication()
