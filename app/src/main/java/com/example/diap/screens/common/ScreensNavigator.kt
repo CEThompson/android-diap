46352f5a18fcfabfe26f9e302753cb4b677230d6
Erik <vwoomdev@gmail.com>
2020-12-30 14:43:37 -0800

    refactor: revert to providing scoped services (ScreensNavigator) within ActivityModule for readability

--- app/src/main/java/com/example/diap/screens/common/ScreensNavigator.kt
+++ app/src/main/java/com/example/diap/screens/common/ScreensNavigator.kt
@@ -1,12 +1,9 @@
 package com.example.diap.screens.common
 
 import androidx.appcompat.app.AppCompatActivity
-import com.example.diap.common.dependencyinjection.activity.ActivityScope
 import com.example.diap.screens.questiondetails.QuestionDetailsActivity
-import javax.inject.Inject
 
-@ActivityScope
-class ScreensNavigator @Inject constructor(private val activity: AppCompatActivity) {
+class ScreensNavigator(private val activity: AppCompatActivity) {
     fun toQuestionDetails(questionId: String) {
         QuestionDetailsActivity.start(activity, questionId)
     }
