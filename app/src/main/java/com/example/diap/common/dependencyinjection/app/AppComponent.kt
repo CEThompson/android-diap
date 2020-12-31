5072f25444f6f2fbb152c9758229cff2b863297b
Erik <vwoomdev@gmail.com>
2020-12-31 14:11:18 -0800

    refactor: set up DI infrastructure for services

--- app/src/main/java/com/example/diap/common/dependencyinjection/app/AppComponent.kt
+++ app/src/main/java/com/example/diap/common/dependencyinjection/app/AppComponent.kt
@@ -2,10 +2,13 @@ package com.example.diap.common.dependencyinjection.app
 
 import com.example.diap.common.dependencyinjection.activity.ActivityComponent
 import com.example.diap.common.dependencyinjection.activity.ActivityModule
+import com.example.diap.common.dependencyinjection.service.ServiceComponent
+import com.example.diap.common.dependencyinjection.service.ServiceModule
 import dagger.Component
 
 @AppScope
 @Component(modules = [AppModule::class])
 interface AppComponent {
     fun newActivityComponent(activityModule: ActivityModule): ActivityComponent
+    fun newServiceComponent(serviceModule: ServiceModule): ServiceComponent
 }
