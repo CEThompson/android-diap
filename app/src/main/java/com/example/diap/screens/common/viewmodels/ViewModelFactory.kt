fa51e0017b1e9ac907ac4bb42a2ce1fbc0d391e8
Erik <vwoomdev@gmail.com>
2021-01-02 13:41:12 -0800

    refactor: change view model factory to instead provide viewmodels with multibindings (note this technique is complicated and may not be worth it, though it does make the viewmodel factory require less maintenance)

--- app/src/main/java/com/example/diap/screens/common/viewmodels/ViewModelFactory.kt
+++ app/src/main/java/com/example/diap/screens/common/viewmodels/ViewModelFactory.kt
@@ -2,21 +2,15 @@ package com.example.diap.screens.common.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.diap.screens.viewmodel.MyViewModel
-import com.example.diap.screens.viewmodel.MyViewModel2
 import javax.inject.Inject
 import javax.inject.Provider
 
-class ViewModelFactory @Inject constructor (
-    private val viewModelProvider: Provider<MyViewModel>,
-    private val viewModel2Provider: Provider<MyViewModel2>
-    ): ViewModelProvider.Factory {
-        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            return when(modelClass){
-                MyViewModel::class.java -> viewModelProvider.get() as T
-                MyViewModel2:: class.java -> viewModel2Provider.get() as T
-                else -> throw RuntimeException("unsupported viewmodel type: $modelClass")
-            }
-
-        }
+class ViewModelFactory @Inject constructor(
+    private val providers: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        val provider = providers[modelClass]
+        return provider?.get() as T
+            ?: throw RuntimeException("unsupported viewmodel type: $modelClass")
+    }
 }
