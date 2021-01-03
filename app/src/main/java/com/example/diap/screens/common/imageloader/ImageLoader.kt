368382dc77ed2b6f250cfbaa9a64c2dceb3710fe
Erik <vwoomdev@gmail.com>
2021-01-03 15:20:39 -0800

    refactor: migrate to hilt scopes

--- app/src/main/java/com/example/diap/screens/common/imageloader/ImageLoader.kt
+++ app/src/main/java/com/example/diap/screens/common/imageloader/ImageLoader.kt
@@ -4,15 +4,15 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import com.example.diap.common.dependencyinjection.activity.ActivityScope
+import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 
-@ActivityScope
-class ImageLoader @Inject constructor (private val activity: AppCompatActivity) {
+@ActivityScoped
+class ImageLoader @Inject constructor(private val activity: AppCompatActivity) {
 
     private val requestOptions = RequestOptions().centerCrop()
 
-    fun loadImage(imageUrl: String, target: ImageView){
+    fun loadImage(imageUrl: String, target: ImageView) {
         Glide.with(activity)
             .load(imageUrl)
             .apply(requestOptions)
